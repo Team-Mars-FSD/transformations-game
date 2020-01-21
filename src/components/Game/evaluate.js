38d632b48ad7a4f24faf2fda1450e064b11368eb
Maria Paz <pazguada@gmail.com>
2020-01-21 16:29:22 -0700

    Added boundary evaluation

--- src/components/Game/evaluate.js
+++ src/components/Game/evaluate.js
@@ -1,15 +1,40 @@
 export const evaluateMatch = (player, goal) => {
-    for (let pCoord in player) {
-        for (let gCoord in goal) {
-            if (player[pCoord].x === goal[gCoord].x && goal[gCoord].y === player[pCoord].y) {
-                console.log(pCoord, gCoord);
-                console.log("x: ",player[pCoord].x, goal[gCoord].x, "y: ", player[pCoord].y, goal[gCoord].y);
-                break;
-            }
-            if (gCoord === "c") {
-                return false;
-            }
-        }
+  for (let pCoord in player) {
+    for (let gCoord in goal) {
+      if (
+        player[pCoord].x === goal[gCoord].x &&
+        goal[gCoord].y === player[pCoord].y
+      ) {
+        console.log(pCoord, gCoord);
+        console.log(
+          "x: ",
+          player[pCoord].x,
+          goal[gCoord].x,
+          "y: ",
+          player[pCoord].y,
+          goal[gCoord].y
+        );
+        break;
+      }
+      if (gCoord === "c") {
+        return false;
+      }
     }
-    return true;
-}
+  }
+  return true;
+};
+
+export const evaluateBoundary = player => {
+  for (let pCoord in player) {
+    if (
+      player[pCoord].x > 10 ||
+      player[pCoord].x < -10 ||
+      player[pCoord].y > 10 ||
+      player[pCoord].y < -10
+    ) {
+      return true;
+      break;
+    }
+  }
+  return false;
+};
